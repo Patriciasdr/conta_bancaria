36a09edf3e3a239005bc1860502bfa759bf447cb
PatriciaSouza <patriciasdr@hotmail.com>
2025-11-10 17:28:43 -0300

    inserção das subclasses ContaPoupanca e ContaCorrente

--- Menu.ts
+++ Menu.ts
@@ -1,12 +1,14 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
 
     let opcao: number;
-
-    // Objeto da Classe Conta (Teste)
+    
+   // Objeto da Classe Conta (Teste)
     const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
     conta.visualizar();
     conta.sacar(10500);
@@ -14,20 +16,21 @@ export function main() {
     conta.depositar(5000);
     conta.visualizar();
 
-    const conta2: Conta = new Conta(2, 124, 1, "Patrícia", 1500);
-    conta2.visualizar();
-    conta2.sacar(2000);
-    conta2.visualizar();
-    conta2.depositar(500);
-    conta2.visualizar();
+   // Objeto da Classe ContaCorrente (Teste)
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
 
-    const conta3: Conta = new Conta(3, 125, 1, "Geovanna", 3000);
-    conta3.visualizar();
-    conta3.sacar(2000);
-    conta3.visualizar();
-    conta3.depositar(3500);
-    conta3.visualizar();
-
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
 
     while (true) {
 
@@ -49,8 +52,7 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("                                                     ", colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
@@ -117,8 +119,8 @@ export function main() {
 function sobre(): void {
     console.log("\n*****************************************************");
     console.log("Projeto Desenvolvido por: ");
-    console.log("Patricia Souza da Rosa - patriciasdrtech@gmail.com");
-    console.log("https://github.com/Patriciasdr");
+    console.log("Generation Brasil - generation@generation.org");
+    console.log("github.com/conteudoGeneration");
     console.log("*****************************************************");
 }
 
